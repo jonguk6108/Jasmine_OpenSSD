f9c23c90f3ab261365bc2a17b8f4e08105c3a2a0
hissubi <jsyoon4642@naver.com>
2021-11-30 01:57:05 +0900

    feat: descriptor

--- not_finished/zns_init_reset_izc.c
+++ not_finished/zns_init_reset_izc.c
@@ -36,9 +36,9 @@ void zns_init(void)
 	*/
 }
 
-void zns_reset(UINT32 lba)
+void zns_reset(UINT32 c_zone)
 {
-	UINT32 c_zone = lba / ZONE_SIZE;
+	UINT32 lba = c_zone / ZONE_SIZE;
 	UINT32 c_bank = (lba / NSECT) % DEG_ZONE;
 	
 	ASSERT(c_zone < NZONE);
@@ -46,11 +46,31 @@ void zns_reset(UINT32 lba)
 	
 	set_zone_state(c_zone, 0);
 	set_zone_wp(c_zone, get_zone_slba(c_zone));
-	//nand_erase
+	nand_block_erase(c_bank, get_zone_to_FBG(c_zone));
 	
 	enqueue_FBG(get_zone_to_FBG(c_zone));
 	set_zone_to_FBG(c_zone, -1);
 
+}
+
+void zns_get_desc(UINT32 c_zone, UINT32 nzone, struct zone_desc *descs)
+{
+	for(UINT i = 0; i < nzone; i++) 
+	{
+		ASSERT(i + c_zone < NZONE);
+
+		if(get_zone_state(i + c_zone) == 3)
+		{
+			descs[i].state = 3;
+			descs[i].slba = get_zone_slba(i + c_zone);
+			descs[i].wp = get_TL_wp(i + c_zone) + get_zone_slba(i + c_zone);
+			continue;
+		}
+		descs[i].state = get_zone_state(i + c_zone);
+		descs[i].slba = get_zone_slba(i + c_zone);
+		descs[i].wp = get_zone_wp(i + c_zone);
+	}
+  return;
 }
 
 void zns_izc(UINT32 src_zone, UINT32 dest_zone, UINT32 copy_len, UINT32 *copy_list)
